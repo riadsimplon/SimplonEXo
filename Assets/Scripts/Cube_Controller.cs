ed8d40279d7f73ed72700c71b39d94ac4f92ab5f
unknown <riadsimplon>
2023-01-09 11:52:20 +0100

    pool d'objet

--- Assets/Scripts/Cube_Controller.cs
+++ Assets/Scripts/Cube_Controller.cs
@@ -9,7 +9,8 @@ public class Cube_Controller : MonoBehaviour
     [SerializeField] private Transform _spawnedOject;
     [SerializeField] private int _limitObject;
     private int _numberObject;
-
+    private Transform _firstListObject;
+    private List<Transform> _myGameObject = new List<Transform>();
 
 
     // Start is called before the first frame update
@@ -18,20 +19,33 @@ public class Cube_Controller : MonoBehaviour
         
 
 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
 
         if (Input.GetMouseButtonDown(0) && _numberObject < _limitObject)
         {
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 25));
             Instantiate(_spawnedOject, worldPosition, Quaternion.identity);
+            _myGameObject.Add(_spawnedOject);
             _numberObject++;
         }
 
+        else if (Input.GetMouseButtonDown(0) && _numberObject == _limitObject)
+        {
+            _firstListObject = _myGameObject[0];
+            Vector3 newWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 25));
+            _firstListObject.position = newWorldPosition;
+            _myGameObject.Add(_spawnedOject);
+            _myGameObject.RemoveAt(0);
+            
+            
 
+
+        }
     }
 }
